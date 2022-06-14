5bf8005fa710a10402619d8b7faeee02378fcf9b
Arizeet Behera <92688073+Arizeet@users.noreply.github.com>
2022-06-14 10:56:23 +0530

    vsb

--- NaN.js
+++ NaN.js
@@ -3,9 +3,14 @@
 // In other words, it is a variable in global scope.
 // The initial value of NaN is not a Number.
 
-var myName="Arizeet";
-var myNumber=9853891497;
-console.log(isNaN(myName));
-console.log(isNaN(myNumber));
+// var myName="Arizeet";
+// var myNumber=9853891497;
+// console.log(isNaN(myName));
+// console.log(isNaN(myNumber));
 
+console.log(isNaN(NaN));
 console.log(NaN===NaN);
+console.log(Number.NaN===NaN);
+console.log(isNaN(Number.NaN===NaN));
+console.log(isNaN(Number.NaN));
+console.log(Number.isNaN(NaN));
